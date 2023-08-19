8637d5d4257e9952446bec8a017e74eab1d36982
EC2 Default User <ec2-user@ip-172-31-10-222.ap-northeast-1.compute.internal>
2023-08-19 14:03:40 +0000

    管理者商品編集

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
 
   validates :amount , presence: true
 
-
+belongs_to :item
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,18 @@ class Item < ApplicationRecord
   validates :introduction , presence: true
   validates :price , presence: true
 
+  has_many :cart_items, dependent: :destroy
+
+  has_one_attached :profile_image#画像用のカラム名
+
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+
+  end
+
 
 end
